5b3edd5d517b470b28f60fdbc161419c0fceb679
pierre lafont <pierre_lafont@hotmail.fr>
2020-09-22 17:51:08 +0200

    Feat: Removed components folder that was generated accidentally during testing

--- src/components/common/button/__tests__/Button.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils';
-import Button from '@/components/button/Button.vue';
-
-describe('Button', () => {
-  test('Is a Vue instance', () => {
-    const wrapper = mount(Button);
-    expect(wrapper.vm).toBeTruthy();
-  });
-});
--- src/components/common/input/__tests__/Input.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils';
-import Input from '@/components/input/Input.vue';
-
-describe('Input', () => {
-  test('Is a Vue instance', () => {
-    const wrapper = mount(Input);
-    expect(wrapper.vm).toBeTruthy();
-  });
-});
